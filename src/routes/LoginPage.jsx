6603bfef1302c9d3aa37e09094df4739d2ebc632
Ng Zheng Jie <zhengjie.zjie@gmail.com>
2025-08-04 08:06:06 +0800

    added login validation

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -1,7 +1,12 @@
+import { RiErrorWarningLine } from "react-icons/ri"
+import { CiWarning } from "react-icons/ci"
 import LoginForm from "@/components/LoginForm"
 import { HiViewBoards } from "react-icons/hi"
+import GlobalContext from "../context/GlobalContext"
+import { useContext } from "react"
 
 function LoginPage() {
+  const ctx = useContext(GlobalContext)
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
@@ -11,6 +16,12 @@ function LoginPage() {
           </div>
           Nimban
         </a>
+        {ctx.loginErrorMsg && (
+          <div className="flex items-center justify-center gap-1 rounded-sm border-1 border-red-300 bg-red-100 p-2 text-red-400">
+            <RiErrorWarningLine className="size-6" />
+            {ctx.loginErrorMsg}
+          </div>
+        )}
         <LoginForm />
       </div>
     </div>
